e2ddd1e2da39e9a9b42f1d9b6c39c15926169370
litcaprice <91788432+litcaprice@users.noreply.github.com>
2022-02-16 01:11:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = rand.Next(-100, 100);
+                Console.Write(array1[i] + " ");
+
+            }
+            Console.Read();
+
+
+
+
+        }
+    }
+}
