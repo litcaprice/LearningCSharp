729c1139826bff7096e9e6051bc9ee875cfd22c6
litcaprice <91788432+litcaprice@users.noreply.github.com>
2022-02-16 01:15:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
@@ -8,15 +8,19 @@ namespace ConsoleApp1
         {
             int[] array1 = new int[20];
             Random rand = new Random();
+            int sum = 0;
             for (int i = 0; i < array1.Length; i++)
             {
-                array1[i] = rand.Next(-100, 100);
+                array1[i] = rand.Next(-100, 101);
                 Console.Write(array1[i] + " ");
-
+                    sum = sum + array1[i];
             }
-            Console.Read();
-
-
+            int average = sum / array1.Length;
+            Console.WriteLine("Sum: " + sum);            // вывод суммы
+            Console.WriteLine("Max: " + array1.Max());   // вывод максимального числа
+            Console.WriteLine("Min: " + array1.Min());   // вывод минимального числа
+            Console.WriteLine("Average: " + average);
+            Console.ReadKey();
 
 
         }
